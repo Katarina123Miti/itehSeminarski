380d3de41d2b653d2f6e8676a62f65738f877fda
Katarina Mitic <kmitic764@gmail.com>
2023-08-05 23:00:16 +0200

    Inicijalizacija

---
+++ Client/src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import {
+  GitHubBanner,
+  Refine,
+  LegacyAuthProvider as AuthProvider,
+} from '@refinedev/core';
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  ReadyPage,
+  ErrorComponent,
+} from '@refinedev/mui';
+import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import AccountCircleOutlined from '@mui/icons-material/AccountCircleOutlined';
+import ChatBubbleOutline from '@mui/icons-material/ChatBubbleOutline';
+import PeopleAltOutlined from '@mui/icons-material/PeopleAltOutlined';
+import StarOutlineRounded from '@mui/icons-material/StarOutlineRounded';
+import VillaOutlined from '@mui/icons-material/VillaOutlined';
+
+
+
+
+
+
+function App() {
+  
+
+  return (
+    
+            
+}
+
+export default App;
